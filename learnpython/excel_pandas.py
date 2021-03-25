d71f16b4d3b5647adf33824e1e2cdebe48d801ba
r2d209@gmail.com <r2d209@gmail.com>
2021-03-25 14:51:53 +0900

    todo excel

--- learnpython/excel_pandas.py
+++ learnpython/excel_pandas.py
@@ -15,3 +15,10 @@ if __name__ == "__main__":
 #pivot
     pivoted = sales.pivot_table(index = 'City', values = 'Sales', aggfunc = 'sum')
     print(pivoted.head())
+
+"""
+1. 각 월정액 상품별 총 시청시간 or 시청건수 : PPM > "월정액구분"을 index로 "시청시간(분)"이나 "시청건수"를 values로 pivot_table적용
+2. 각 월정액 내 CP별 총 시청기여율 : PPM > "월정액구분" 리스트 중 1개씩 필터링 > 해당 월정액 내 "거래처명" 리스트 중 1개씩 필터링 > 시청시간이나 시청건수를 sum
+3. 월정액 별 CP의 시청기여율 : 2에서 추출된 월정액 내 CP별 총 시청시간 / 1에서 추출된 각 월정액별 총 시청시간 x 100
+여기에 1, 2, 3의 내용을 각 엑셀 back data로 별도 저장해 분류 제공 필요
+"""
